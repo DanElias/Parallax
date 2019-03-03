8bd819671a609b73f91bef9b11963ce5db35b0ef
JoseP <A01702828@itesm.mx>
2019-03-03 16:18:28 -0600

    Pagina de login con sesiones de PHP, la sesione sta en util.php

--- Proyecto/Website/login.php
+++ Proyecto/Website/login.php
@@ -1,32 +1,31 @@
 <?php
+
+//Inicio de sesion
 require_once("util.php");
 session_start();
 
+//Condicionales de validacion de email
 if(isset($_POST["email"]) && isset($_POST["password"])) {
 
-    $usuario = login(htmlspecialchars($_POST["email"]), htmlspecialchars($_POST["password"]));
-    if ($usuario != "") {
-        $_SESSION["usuario"] = $usuario;
-        header("location:admin.php");
-    } else {
-        $error = "Usuario y/o contraseña incorrectos";
-        include("_login.html");
-    }
-} else {
-    $error = "Usuario y/o contraseña incorrectos";
-    include("_login.html");
-}
-if(isset($_POST["submit"])) {
     $_POST['email'] = htmlentities($_POST['email']);
     $_POST['password'] = htmlentities($_POST['password']);
 
-}else {
+    $usuario = login(htmlspecialchars($_POST["email"]), htmlspecialchars($_POST["password"]));
+    if ($usuario != "") {
+        $_SESSION["usuario"] = $usuario; //Asignacion de usaurio y contraseña en util.php
+        header("location:admin.php");
+    } else {
+        $error = "Usuario y/o contraseña incorrectos";
+        header_html();
+        include("_login.html");
+        include("views/_footer_login.html");
+    }
+    //Si es la priemra vez , solo mostrara los datos
+} else {
     header_html();
     include("_login.html");
     include("views/_footer_login.html");
 }
-
-
 
 
 ?>
