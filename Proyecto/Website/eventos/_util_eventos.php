ffd96a79494ce3e6634153229c7ba9fb5032913b
Dan <A01208905@itesm.mx>
2019-03-11 20:23:11 -0600

    Añadir phps

---
+++ Proyecto/Website/eventos/_util_eventos.php
@@ -0,0 +1,15 @@
+<?php
+
+function header_html($titulo="MS - Admin") {
+    include("../views/_header_admin.html");
+}
+
+function sidenav_html(){
+    include("../views/_sidenav_admin.html");
+}
+
+function footer_html(){
+    include("../views/_footer_admin.html");
+}
+
+?>
