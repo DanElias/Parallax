3e69d14304423127daf8bf7bcd96d40bdd7b7037
Daniel Elias <a01208905@itesm.mx>
2019-03-22 03:51:41 +0000

    Cambiar modals

--- Proyecto/Website/basesdedatos/_conection_queries_db.php
+++ Proyecto/Website/basesdedatos/_conection_queries_db.php
@@ -5,7 +5,7 @@
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "logindb";
+        $dbname = "proyecto";
         
         $con = mysqli_connect($servername, $username, $password, $dbname);
         
