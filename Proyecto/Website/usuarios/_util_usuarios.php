472b932bab4ce6ccbcb7b5006fa5fede2c1ba1a4
JoseP <A01702828@itesm.mx>
2019-03-23 13:40:56 -0600

    Form usaurio completo

--- Proyecto/Website/usuarios/_util_usuarios.php
+++ Proyecto/Website/usuarios/_util_usuarios.php
@@ -1,36 +1,44 @@
 <?php
+
 function header_html($titulo = "Usuarios")
 {
-include("../views/_header_admin.html");
+    include("../views/_header_admin.html");
 }
 
 function sidenav_html()
 {
-include("../views/_sidenav_admin.html");
+    include("../views/_sidenav_admin.html");
 }
 
 function footer_html()
 {
-include("../views/_footer_admin.html");
+    include("../views/_footer_admin.html");
 }
 
-function body_usuarios()
+function body_proveedores()
 {
-include("_usuarios.html");
+    include("_usuarios.html");
 }
+
 function form_agregar_usuario()
 {
- include("forms/_form_agregar_usuario.html");
-}
-function form_editar_usuario()
-{
- include("forms/_form_editar_usuario.html");
-}
-function form_eliminar_usuario()
-{
- include("forms/_form_eliminar_usuario.html");
+    include("forms/_form_agregar_usuario.html");
 }
 
+function form_editar_usuario()
+{
+    include("forms/_form_editar_usuario.html");
+}
+
+function form_eliminar_usuario()
+{
+    include("forms/_form_eliminar_usuario.html");
+}
+
+function modal_informacion_usuario()
+{
+    include("_modal_informacion_usuario.html");
+}
 
 
 ?>
