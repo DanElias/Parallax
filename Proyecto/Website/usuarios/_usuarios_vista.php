f50e2803dd7cf494e9390bb24deb7c8c1d2c46a7
JoseP <A01702828@itesm.mx>
2019-03-23 13:12:51 -0600

    Forms de agregar usuario

--- Proyecto/Website/usuarios/_usuarios_vista.php
+++ Proyecto/Website/usuarios/_usuarios_vista.php
@@ -6,8 +6,11 @@ if (isset($_SESSION["usuario"])) {
     header_html();
     sidenav_html();
     body_usuarios();
+    form_agregar_usuario();
+    form_editar_usuario();
+    form_eliminar_usuario();
 
-    
+
 } else {
     header("location:../login/_login_vista.php");
 
--- Proyecto/Website/usuarios/_util_usuarios.php
+++ Proyecto/Website/usuarios/_util_usuarios.php
@@ -18,5 +18,19 @@ function body_usuarios()
 {
 include("_usuarios.html");
 }
+function form_agregar_usuario()
+{
+ include("forms/_form_agregar_usuario.html");
+}
+function form_editar_usuario()
+{
+ include("forms/_form_editar_usuario.html");
+}
+function form_eliminar_usuario()
+{
+ include("forms/_form_eliminar_usuario.html");
+}
+
+
 
 ?>
