63a25704d8888cac571538f3e2f2ca535e7a7b15
hugoTec <hfranco21@hotmail.com>
2019-03-27 01:12:59 -0600

    avanzar beneficiario

--- Proyecto/Website/basesdedatos/_conection_queries_db.php
+++ Proyecto/Website/basesdedatos/_conection_queries_db.php
@@ -25,82 +25,82 @@
 
  //obtiene todas las tuplas con todos sus datos
     function obtenerEventos(){
-        
+
         $conn = conectDb();
-        
+
         $sql = "SELECT id_evento, nombre, fecha, hora, lugar, descripcion, imagen FROM evento";
-        
+
         $result = mysqli_query($conn, $sql);
-        
+
         closeDb($conn);
-        
+
         return $result;
     }
 
     // regresa todos los datos de una tupla cuyo nombre sea igual al especificado
     function obtenerEventosPorNombre($nombre_evento){
         $conn = conectDb();
-        
+
         $sql = "SELECT id_evento, nombre, fecha, hora, lugar, descripcion, imagen FROM evento WHERE nombre LIKE '%".$nombre_evento."%'";
-        
+
         $result = mysqli_query($conn, $sql);
-        
+
         closeDb($conn);
-        
+
         return $result;
     }
 
     // regresa todos los datos de una tupla cuyo fecha ocurra antes o igual a una fecha especificada.
     function obtenerEventosPorFecha($fecha_evento){//Las fechas se guardan como 1998-03-28
         $conn = conectDb();
-        
+
         $sql = "SELECT id_evento, nombre, fecha, hora, lugar, descripcion, imagen FROM evento WHERE fecha LIKE '%".$fecha_evento."%'";
-        
+
         echo $sql;
-        
+
         $result = mysqli_query($conn, $sql);
-        
+
         closeDb($conn);
-        
+
         return $result;
     }
-    
+
     function obtenerEventosPorID($id_evento){
-        
+
         $conn = conectDb();
-        
+
         $sql = "SELECT id_evento, nombre, fecha, hora, lugar, descripcion, imagen FROM evento WHERE id_evento = '".$id_evento."'";
-        
+
         $result = mysqli_query($conn, $sql);
-        
+
         closeDb($conn);
-        
+
         return $result;
     }
-    
+
     function obtenerEventoReciente(){
-        
+
         $conn = conectDb();
-        
+
         $sql = "SELECT id_evento FROM evento ORDER BY id_evento DESC LIMIT 1";
-        
+
         $result = mysqli_query($conn, $sql);
-        
+
         closeDb($conn);
-        
+
         return $result;
     }
-    
+
     //Inserta un nuevo evento en la base de datos
     function insertarEvento($nombre_evento,$fecha_evento, $hora_evento, $lugar_evento, $descripcion_evento, $imagen_evento){
         $conn=conectDb();
-        
-        
+
+
         //INSERT INTO evento VALUES (,'Venta de Garage' ,'2019-03-28', '16:30:00','Mariana Sala I.A.P.' ,'Vamos a vender mobiliario para obtener fondos.', '../eventos/uploads/f3.jpg')
-        
-        $sql="INSERT INTO evento (id_evento, nombre, fecha, hora, lugar, descripcion, imagen) VALUES 
+
+        $sql="INSERT INTO evento (id_evento, nombre, fecha, hora, lugar, descripcion, imagen) VALUES
         (0,\"".$nombre_evento."\",\"".$fecha_evento."\",\"".$hora_evento."\",\"".$lugar_evento."\",\"". $descripcion_evento."\",\"".$imagen_evento."\")";
-        
+
         if(mysqli_query($conn,$sql)){
             closeDb($conn);
             return true;
@@ -109,16 +109,16 @@
             closeDb($conn);
             return false;
         }
-        
+
     }
-    
+
     //Inserta un nuevo evento en la base de datos
     function editarEvento($id_evento, $nombre_evento,$fecha_evento,$hora_evento,$lugar_evento, $descripcion_evento, $imagen_evento){
         $conn=conectDb();
-        
+
         $sql="UPDATE evento SET id_evento=".$id_evento.", nombre='".$nombre_evento."', fecha='".$fecha_evento."', hora='".$hora_evento."', lugar='".$lugar_evento."', descripcion='".$descripcion_evento."', imagen='".$imagen_evento."'
         WHERE id_evento=".$id_evento."";
-        
+
         if(mysqli_query($conn,$sql)){
             closeDb($conn);
             return true;
@@ -127,15 +127,15 @@
             closeDb($conn);
             return false;
         }
-        
+
     }
-    
-    //borra el evento que tenga el id 
+
+    //borra el evento que tenga el id
     function eliminarEventoPorID($id_evento){
         $conn = conectDb();
-        
+
         $sql = "DELETE FROM evento WHERE id_evento ='".$id_evento."'";
-        
+
         if(mysqli_query($conn,$sql)){
             closeDb($conn);
             return true;
@@ -167,12 +167,12 @@
         $conn = conectDb();
         //$sql = "INSERT INTO Proveedor(rfc,alias,razon_social,nombre_contacto,telefono_contacto,cuenta_bancaria, banco) VALUES (\"".$rfc."\",\"".$alias."\",\"".$razon."\",\"".$nombre."\",\"".$telefono."\",\"".$cuenta."\",\"".$banco."\")";
         //Query de SQl para insertar en la tabla de proveedores
-         $sql = "INSERT INTO Proveedor(rfc,alias,razon_social,nombre_contacto,telefono_contacto,cuenta_bancaria, banco) 
+         $sql = "INSERT INTO Proveedor(rfc,alias,razon_social,nombre_contacto,telefono_contacto,cuenta_bancaria, banco)
                 VALUES ('$rfc', '$alias','$razon', '$nombre', '$telefono', '$cuenta', '$banco')";
 
-    
+
         //'$rfc','$alias','$razon','$nombre','$telefono','$cuenta','$banco''
-        
+
         if(mysqli_query($conn,$sql)){
             closeDb($conn);
             return true;
@@ -180,7 +180,7 @@
             closeDb($conn);
             return false;
         }
- 
+
     }
 
     function registrar_cuenta_contable($nombre_cuenta,$descripcion_cuenta){
@@ -242,7 +242,20 @@ function obtenerUsuario(){
     return $result;
 }
 
+function registrar_tutor($nombre,$fecha,$telefono,$ocupacion,$empresa,$titulo,$grado){
 
+    $conn = conectDb();
+    $sql = "INSERT INTO tutor(nombre,telefono,fecha_nacimiento,ocupacion,nombre_empresa,grado_estudio,titulo_obtenido) VALUES ('".$nombre."','".$fecha."','".$telefono."','".$ocupacion."','".$empresa"','".$titulo."','".$grado."')";
+    if(mysqli_query($conn,$sql)){
+        closeDb($conn);
+        return true;
+    }
+    else{
+        closeDb($conn);
+        return false;
+    }
+    
+}
 
 
 ?>
