2c6083f2b3040c6ee22b7d51605ce353a5915665
hugoTec <hfranco21@hotmail.com>
2019-06-03 20:35:25 -0500

    modificar controlador login

--- Proyecto/Website/login/_controlador_login.php
+++ Proyecto/Website/login/_controlador_login.php
@@ -6,53 +6,58 @@ require_once("_util_login.php");
 session_start();
 
  if (isset($_POST["submit"])){
-     
+
     $_POST['email'] = htmlentities($_POST['email']);
     $_POST['password'] = htmlentities($_POST['password']);
-    
-  
-if (isset($_POST["email"]) && isset($_POST["password"])) {
-    
-
-    //Datos que va a tomar, validar con htmltiteies
-    $_POST['email'] = htmlentities($_POST['email']);
-    $_POST['password'] = htmlentities($_POST['password']);
-    
-    $_SESSION['error_bd_login']=1;
-    
-    $usuario = login($_POST["email"], $_POST["password"]);
-    
-    
-    if (autentificarse(($_POST["email"]), ($_POST["password"]))) {
-        
-        //echo '<script>alert("fuck");</script>';
-        //Te manda a location de admin
-        header("location:../admin/_admin_vista.php");
-        
-        $_SESSION['error_bd_login']=0;
-
-        //Si si existe , saca el nombre de la sesion (correo y contraseñas)
-        if (mysqli_num_rows($usuario)) {
-            while ($row = mysqli_fetch_assoc($usuario)) {
- 	
-                //asigna a sesion el nombre de la personas
-                if(!isset($_SESSION['usuario'])){
-                	$_SESSION['usuario'] = $row['nombre'];
-                }
-                else{
-                	$_SESSION['usuario'] = $row['nombre'];
-                }
-                $_SESSION['id_rol'] = $row['id_rol'];
-                $_SESSION['id_usuario'] = $row['id_usuario'];
-                if($GLOBALS['local_servidor'] == 1){
-                    echo'<script type="text/javascript">
-		window.location="https://www.marianasala.org/Website/admin/_admin_vista.php";
-		</script>';
-                }
 
 
-            }
-        }
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+
+
+      //Datos que va a tomar, validar con htmltiteies
+      $_POST['email'] = htmlentities($_POST['email']);
+      $_POST['password'] = htmlentities($_POST['password']);
+
+      $_SESSION['error_bd_login']=1;
+
+      $usuario = login($_POST["email"], $_POST["password"]);
+
+
+      if (autentificarse(($_POST["email"]), ($_POST["password"]))) {
+
+          //Te manda a location de admin
+          //header("location:../admin/_admin_vista.php");
+
+          $_SESSION['error_bd_login']=0;
+
+          //Si si existe , saca el nombre de la sesion (correo y contraseñas)
+          if (mysqli_num_rows($usuario) == 1) {
+              while ($row = mysqli_fetch_assoc($usuario)) {
+
+                  //asigna a sesion el nombre de la personas
+                  if(!isset($_SESSION['usuario'])){
+                  	$_SESSION['usuario'] = $row['nombre'];
+                  }
+                  else{
+                  	$_SESSION['usuario'] = $row['nombre'];
+                  }
+                  $_SESSION['id_rol'] = $row['id_rol'];
+                  $_SESSION['id_usuario'] = $row['id_usuario'];
+                  /*if($GLOBALS['local_servidor'] == 1){
+                      echo'<script type="text/javascript">
+  		window.location="https://www.marianasala.org/Website/admin/_admin_vista.php";
+  		</script>';*/
+              }
+                header("location:../admin/_admin_vista.php");
+          } else {
+              $_SESSION['error_bd_login']=0;
+              $error = "El usuario aparece más de una vez, contacte al administradorS";
+              header_html();
+              include("_login.html");
+              include("../views/_footer_login.html");
+          }
+
+
     } else {
         //Reedireccion a login si no esta la sesion iniciada
         $_SESSION['error_bd_login']=0;
@@ -62,18 +67,12 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         include("../views/_footer_login.html");
     }
 
-    //Si es la primera vez , solo mostrara los datos
-} else {
+  } else {
     $_SESSION['error_bd_login']=0;
+    $error = "Usuario y/o contraseña vacios";
     header_html();
     include("_login.html");
     include("../views/_footer_login.html");
-}
-} else {
-    $_SESSION['error_bd_login']=0;
-    header_html();
-    include("_login.html");
-    include("../views/_footer_login.html");
-}
+  }
 
-$_SESSION['error_bd_login']=0;
+}
--- Proyecto/Website/login/_login_vista.php
+++ Proyecto/Website/login/_login_vista.php
@@ -2,9 +2,11 @@
 
 //Inicio de sesion
 require_once("_util_login.php");
-require_once("_controlador_login.php");
+//require_once("_controlador_login.php");
 session_start();
-
+header_html();
+include("_login.html");
+include("../views/_footer_login.html");
 //Se debe tener email y password con algo lleno para pasar a la sigueinte validacion
 
 
